74e2f20d74f31c24e4b86001b0aed5a14515f74e
ShreyanshKunjera <shreyanshkunjera@gmail.com>
2024-02-01 22:21:50 +0530

    Proper commenting the leap_year.py file in LeapYear branch

--- leap_year.py
+++ leap_year.py
@@ -7,20 +7,22 @@ def main():
 	# If user give year < 1900 then tell them to reenter the valid year and terminate the program
 	if year<1900:
 		print("Enter the year greater than 1900")
-		return -1
+		return -1 # return -1 to terminate the code in case of wrong input
 	elif year>100000:
 		print("Enter the year less than 100000")
 		return -1
-	print(is_leap(year))
+	print(is_leap(year)) # calling the function and printing the output of true or false
 
 
 # Defining the function to check if the year is leap year or not
 def is_leap(year):
+	#if year is not a century then it should be divided by 4 to be a leap year
+	#if year is a century then it should be divided by 400 to be a leap year
 	if (year%100!=0 and year%4!=0) or (year%100==0 and year%400!=0):
 		return False
 	else:
 		return True
 
-
+# Calling the main function
 if __name__=='__main__':
 	main()
