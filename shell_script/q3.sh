a77c17e8c44170a93a5b86f916a602781a96b6f8
ShreyanshKunjera <shreyanshkunjera@gmail.com>
2024-02-02 15:47:15 +0530

    Adding q3.sh file to shell_script folder first time

---
+++ shell_script/q3.sh
@@ -0,0 +1,10 @@
+# Given two integers, X and Y, find their sum, difference, product, and quotient.
+
+echo 'Enter the values of x and y:'
+read a 
+read b
+
+echo "sum is : $(( a + b ))"
+echo "difference is : $(( a - b ))"
+echo "product is : $(( a * b ))"
+echo "quotient is : $(( a / b ))"
