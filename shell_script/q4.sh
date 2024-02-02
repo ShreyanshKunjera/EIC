1f7131f94731439e10e759e45b7f4e3fd3b12ac0
ShreyanshKunjera <shreyanshkunjera@gmail.com>
2024-02-02 15:47:25 +0530

    Adding q4.sh file to shell_script folder first time

---
+++ shell_script/q4.sh
@@ -0,0 +1,15 @@
+# Given two integers, X and Y, identify whether X > Y or X < Y or X = Y.
+
+echo 'Enter the values of X and Y:'
+read X
+read Y
+
+if [[ $X -gt $Y ]]
+then
+	echo "X is greater than Y"
+elif [ $X -lt $Y ]
+then
+	echo "X is less than Y"
+else
+	echo "X is equal to Y"
+fi
